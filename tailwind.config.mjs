4b87c1057ba5f32400d68196575ebafb4e971cc1
Abajit Dey <abajitdeyag10118@gmail.com>
2024-12-12 23:30:14 +0530

    First commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,31 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        brand_primary: "#3D52A0",
+        brand_secondary: "#7091E6",
+        brand_accent: "#FF00FF",
+        brand_white: "#EDE8F5"
+      },
+      animation: {
+        'spin-slow': 'spin 10s linear infinite'
+      },
+      keyframes: {
+        spin: {
+          'from': { transform: 'rotate(0deg)' },
+          'to': { transform: 'rotate(360deg)'}
+        }
+      },
+      fontFamily: {
+        sans: [
+          'Inter', ...defaultTheme.fontFamily.sans,
+        ]
+      }
+    },
+  },
+  plugins: [],
+};
